ba5b374b63d246235e9bedb47abda1ae8c53b086
W. Ely Paysinger <paysinger@gmail.com>
2017-07-24 16:19:26 -0700

    working on cors

--- game_ai/bot/views.py
+++ game_ai/bot/views.py
@@ -13,4 +13,12 @@ from django.views.decorators.csrf import csrf_exempt
 def bot_play(request):
     """Api end pont for bot play."""
     data = json.loads(request.body)
+    import pdb; pdb.set_trace()
+    # request.response.headerlist.extend(
+    #     (
+    #         ('Access-Control-Allow-Origin',
+    #          '*'),
+    #         ('Content-Type', 'application/json')
+    #     )
+    # )
     return JsonResponse(tic_tack.directory(data['board'], data['move']))
