96a33087ca9a405c0e6fe4366004eb2a19994d89
SeanBeseler <seanwbeseler@gmail.com>
2017-07-24 14:44:14 -0700

    django done for now

---
+++ game_ai/bot/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from bot import tic_tack
+import json
+from django.views.decorators.csrf import csrf_exempt
+
+
+
+# Create your views here.
+
+
+@csrf_exempt
+def bot_play(request):
+    """Api end pont for bot play."""
+    data = json.loads(request.body)
+    return JsonResponse(tic_tack.directory(data['board'], data['move']))
