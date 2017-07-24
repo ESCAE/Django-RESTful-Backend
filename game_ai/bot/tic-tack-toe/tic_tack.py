1b830a29678d3515e17692cef66e4ff8f8ceb4df
SeanBeseler <seanwbeseler@gmail.com>
2017-07-24 13:27:52 -0700

    core rules added

---
+++ game_ai/bot/tic-tack-toe/tic_tack.py
@@ -0,0 +1,231 @@
+#from random import randint
+
+
+#def make_board(board):
+  #  mid_line = '------\n'
+    #output = ''
+   # for x in range(9):
+     #   if board[x] not in ['X', 'O']:
+        #    output += ' '
+        #else:
+          #  output += board[x]
+        #if x in [2, 5]:
+          #  output += '\n'
+            #output += mid_line
+        #elif x != 8:
+          #  output += '|'
+   # print(output)
+
+
+def check_move(board, move):
+    if board[move] == ' ':
+        return True
+    return False
+
+
+def winner(board, player):
+    if board[0] == player and board[1] == player and board[2] == player:
+        if player == 'X':
+            return True
+        return False
+    if board[0] == player and board[3] == player and board[6] == player:
+        if player == 'X':
+            return True
+        return False
+    if board[0] == player and board[4] == player and board[8] == player:
+        if player == 'X':
+            return True
+        return False
+    if board[1] == player and board[4] == player and board[7] == player:
+        if player == 'X':
+            return True
+        return False
+    if board[2] == player and board[5] == player and board[8] == player:
+        if player == 'X':
+            return True
+        return False
+    if board[2] == player and board[4] == player and board[6] == player:
+        if player == 'X':
+            return True
+        return False
+    if board[3] == player and board[4] == player and board[5] == player:
+        if player == 'X':
+            return True
+        return False
+    if board[6] == player and board[7] == player and board[8] == player:
+        if player == 'X':
+            return True
+        return False
+
+
+def new_board(board, move, chariter='X'):
+    output = []
+    output_send = {'board': '', 'move': move, 'WL': None}
+    output_str = ''
+    for x in range(len(board)):
+        if x != move:
+            output.append(board[x])
+            output_str += board[x]
+        else:
+            output.append(chariter)
+            if chariter is 'X':
+                output_str += chariter
+            else:
+                output_str += board[x]
+    output_send['board'] = output_str
+    output_send['WL'] = winner(output, chariter)
+    return output_send
+
+
+def directory(board, move):
+    board_list = []
+    for x in board:
+        board_list.append(x)
+    board_dic = new_board(board_list, move)
+    if board_dic['WL'] is not None:
+        return board_dic
+    board_list = []
+    for x in board_dic['board']:
+        board_list.append(x)
+    greedy_move = greedy_bot(board_list)
+    board_dic = new_board(board_list, greedy_move ,'O')
+    return board_dic
+
+
+def new_board_bot(board, move, chariter='X'):
+    output = []
+    for x in board:
+        output.append(x)
+    output[move] = chariter
+    return output
+
+
+def greedy_bot(board):
+    wins = []
+    count = 0
+    for a in range(len(board)):
+        count += 1
+        wins.append(-1)
+        if board[a] == ' ':
+            wins[a] = 0
+            newboard_0 = new_board_bot(board, a, 'O')
+            if not winner(newboard_0, 'O'):
+                return a
+            for s in range(len(board)):
+                count += 1
+                if newboard_0[s] == ' ':
+                    newboard_1 = new_board_bot(newboard_0, s, 'X')
+                    if winner(newboard_1, 'X'):
+                        return s
+                    for d in range(len(board)):
+                        count += 1
+                        if newboard_1[d] == ' ':
+                            newboard_2 = new_board_bot(newboard_1, d, 'O')
+                            if not winner(newboard_2, 'O'):
+                                wins[a] += 1
+                            else:
+                                for f in range(len(board)):
+                                    count += 1
+                                    if newboard_2[f] == ' ':
+                                        newboard_3 = new_board_bot(newboard_2, f, 'X')
+                                        if winner(newboard_3, 'X'):
+                                            wins[a] -= 1
+                                        else:
+                                            for g in range(len(board)):
+                                                count += 1
+                                                if newboard_3[g] == ' ':
+                                                    newboard_4 = new_board_bot(newboard_3, g, 'O')
+                                                    if not winner(newboard_4, 'O'):
+                                                        wins[a] += 1
+                                                    else:
+                                                        for q in range(len(board)):
+                                                            if newboard_4[q] == ' ':
+                                                                newboard_5 = new_board_bot(newboard_4, q, 'X')
+                                                                if winner(newboard_5, 'X'):
+                                                                    wins[a] -= 1
+                                                                else:
+                                                                    for w in range(len(board)):
+                                                                        if newboard_5[w] == ' ':
+                                                                            newboard_6 = new_board_bot(newboard_5, w, 'O')
+                                                                            if not winner(newboard_6, 'O'):
+                                                                                wins[a] += 1
+                                                                            else:
+                                                                                for e in range(len(board)):
+                                                                                    if newboard_6[e] == ' ':
+                                                                                        newboard_7 = new_board_bot(newboard_6, e, 'X')
+                                                                                        if winner(newboard_7, 'X'):
+                                                                                            wins[a] -= 1
+                                                                                        else:
+                                                                                            for r in range(len(board)):
+                                                                                                if newboard_7[r] ==  ' ':
+                                                                                                    newboard_8 = new_board_bot(newboard_7, r, 'O')
+                                                                                                    if not winner(newboard_8, 'O'):
+                                                                                                        wins[a] += 1
+    the_max = float('-inf')
+    total = 0
+    for x in range(len(wins)):
+        if the_max <= wins[x] and check_move(board, x):
+            the_max = wins[x]
+            total = x
+    return total
+
+
+def dumb_bot(board):
+    while True:
+        move = randint(0, 8)
+        if board[move] == ' ':
+            break
+        print(move)
+    return move
+
+
+#def play_game():
+    #board = [' ', ' ', ' ', ' ', ' ',  ' ', ' ',  ' ',  ' ']
+    #player = randint(1, 2)
+    #want_bot = 'Y'
+    #move = ''
+    #if want_bot == 'Y':
+        #want_bot = True
+    #else:
+        #want_bot = False
+    #turn = 0
+    #while True:
+        #turn += 1
+        #if want_bot and player == 1:
+            #while True:
+                #output = 'It is player ' + str(player) + ' turn enter a move'
+                #move = input(output)
+                #if check_move(board, int(move)):
+                    #break
+                #print('Move is not vaild, please enter a vaild move ')
+        #else:
+            #move = greedy_bot(board)
+        #if player == 1:
+            #board[int(move)] = 'X'
+        #else:
+            #board[int(move)] = 'O'
+        #if player == 1:
+            #if winner(board, 'X'):
+                #make_board(board)
+                #output = 'player ' + str(player) + ' has won'
+                #print(output)
+                #break
+        #else:
+            #if winner(board, 'O'):
+                #make_board(board)
+                #output = 'player ' + str(player) + ' has won'
+                #print(output)
+                #break
+
+        #make_board(board)
+        #print('\n')
+        #if player == 1:
+            #player = 2
+        #else:
+            #player = 1
+        #if turn == 9:
+            #print('It is a tie, nobody wins')
+            #break
+
+
+print(directory('         ', 3))
