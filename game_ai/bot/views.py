13a14e3a38f91f13ac6d82cbf38c60d6b800bdac
W. Ely Paysinger <paysinger@gmail.com>
2017-07-24 16:35:12 -0700

    working on cors

--- game_ai/bot/views.py
+++ game_ai/bot/views.py
@@ -14,11 +14,11 @@ def bot_play(request):
     """Api end pont for bot play."""
     data = json.loads(request.body)
     import pdb; pdb.set_trace()
-    # request.response.headerlist.extend(
-    #     (
-    #         ('Access-Control-Allow-Origin',
-    #          '*'),
-    #         ('Content-Type', 'application/json')
-    #     )
-    # )
+    request.response.headerlist.extend(
+        (
+            ('Access-Control-Allow-Origin',
+             '*'),
+            ('Content-Type', 'application/json')
+        )
+    )
     return JsonResponse(tic_tack.directory(data['board'], data['move']))
