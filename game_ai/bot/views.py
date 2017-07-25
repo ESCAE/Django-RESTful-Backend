595e8f6f8dd2ab3cb3175dcb4bec2b3586939341
W. Ely Paysinger <paysinger@gmail.com>
2017-07-24 17:33:56 -0700

    added home route

--- game_ai/bot/views.py
+++ game_ai/bot/views.py
@@ -3,6 +3,8 @@ from django.http import JsonResponse
 from bot import tic_tack
 import json
 from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse
+
 
 
 
@@ -14,3 +16,8 @@ def bot_play(request):
     """Api end pont for bot play."""
     data = json.loads(request.body)
     return JsonResponse(tic_tack.directory(data['board'], data['move']))
+
+
+def home(request):
+    """Api end pont for bot play."""
+    return HttpResponse("<h1>Hello World!</h1>")
